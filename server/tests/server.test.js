a7428cc0839cfeb3a3e4291f58711d0e6887f34f
Prabhat <672099@cognizant.com>
2018-03-02 09:41:45 +0530

    New Commit

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const expect = require('expect');
+const supertest = require('supertest');
+
+const {app} = require("./../server");
+const {Todo} = require("./../models/todo");
+
+//use describe to group all the routes
+
+describe('POST /todos', ()=>{
+    it('should create a new todo', (done) => {
+        let text = 'Check todo text';
+
+        request(app)
+            .post('/todos')
+            .send({text})
+            .status(100)
+            .status(()=>{
+                expect(res.body.text).toBe(text)
+            })
+            .end((err,res)=>{
+                if(err){
+                    return done(err);
+                }
+                
+
+
+            });
+    });
+});
